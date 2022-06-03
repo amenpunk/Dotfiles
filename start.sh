10247254c89380d0838c286a60ef3da393139626
amenpunk <ondasycircuitos@gmail.com>
2022-06-03 17:26:07 -0600

    automatic shit alv

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-
+autorandr --change
+xset r rate 300 60
+warpd
 setxkbmap -option caps:swapescape
 setxkbmap -layout us,latam
 setxkbmap -option 'grp:alt_shift_toggle'
-xset r rate 300 60
-warpd
-wmfocus
+nitrogen --restore
